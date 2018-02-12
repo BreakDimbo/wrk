e76653f5b5f4f66080152fb5b377be5479713356
董昊 <donghao@udesk.cn>
2018-02-12 15:39:30 +0800

    fix

--- scripts/benchmark.lua
+++ scripts/benchmark.lua
@@ -4,20 +4,23 @@
 -- NOTE: each wrk thread has an independent Lua scripting
 -- context and thus there will be one counter per thread
 
-request = function()
-   local base = require("base") 
-   local sha1 = require("sha1")
-   local method = "GET"
-   local time_stamp = os.date("%Y%m%d%H%M%S", os.time())
-   local user_name = "123456789abcdef"
-   local auth_token="a61b82d5497f7a31b5f069c8bade"
-   local sign_str = sha1(user_name..auth_token..time_stamp)
-   local app_id = "14118cc8-7d46-4868-567d-e22179bb3408"
-   local apigtw_host = "/cmd/v1/"
-   local authorization = enc(user_name..":"..sign_str)
+init = function (args)
+  local base = require("base") 
+  local sha1 = require("sha1")
+  local time_stamp = os.date("%Y%m%d%H%M%S", os.time())
+  local user_name = "123456789abcdef"
+  local auth_token="a61b82d5497f7a31b5f069c8bade"
+  local sign_str = sha1(user_name..auth_token..time_stamp)
+  local app_id = "14118cc8-7d46-4868-567d-e22179bb3408"
+  local apigtw_host = "/cmd/v1/"
 
-   path = apigtw_host.."?timestamp="..time_stamp.."&app_id="..app_id
+  authorization = enc(user_name..":"..sign_str)
+  method = "GET"
+  path = apigtw_host.."?timestamp="..time_stamp.."&app_id="..app_id
+end
+
+
+request = function()
    wrk.headers["Authorization"] = "Basic "..authorization
-   
    return wrk.format(method, path, nil, body)
 end
